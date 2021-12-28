6b0ea44f7fc6d89983d1a813a5e1e8bd5300b911
Damilola Mohammed <damilolamohammed2@hotmail.com>
2021-12-28 23:10:08 +0100

    7kyu updated

---
+++ Kyu 7/35.js
@@ -0,0 +1,14 @@
+/*
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+*/
+function flattenAndSort(array) {
+    // Good luck, brave code warrior!
+    let result = []
+    array.forEach( e => result = result.concat(...e) )
+    result = result.sort( (a,b) => a - b )
+    return result;
+  }
